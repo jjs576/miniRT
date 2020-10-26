0cbc35674e294fe8e65145413302a384dedd9878
Jisoo Joo <jjoo@c3r7s1.42seoul.kr>
2020-10-26 18:32:15 +0900

    refactor everything :(

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 17:49:29 by jjoo              #+#    #+#             */
-/*   Updated: 2020/10/26 18:07:14 by jjoo             ###   ########.fr       */
+/*   Updated: 2020/10/26 18:32:09 by jjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,25 @@ int			open_file(char *path)
 
 static int	get_next_line(int fd, char **line)
 {
-	char	*line;
+	char	*save;
 	char	buf[2];
+	char	*temp;
 	int		flag;
 
-	if (fd == -1)
-		return E_CANNOT_OPEN;
-	while ((flag = read(fd, buf, 1) >= 0)
+	if (fd < 0 || !line)
+		return R_GNL_ERROR;
+	save = ft_calloc(0, 1);
+	while ((flag = read(fd, buf, 1)) >= 0)
 	{
-		buf[1] = 0;
-
+		buf[flag] = 0;
+		temp = save;
+		save = ft_strjoin(temp, buf);
+		free(temp);
+		temp = 0;
+		if (flag == 0)
+			break ;
 	}
+	return (flag);
 }
 
 int			read_file(int fd, t_my_mlx *mlx)
